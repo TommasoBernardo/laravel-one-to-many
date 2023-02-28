4306a0c9e337b10894f484da4c770676ecd76e66
TommasoBernardo <tommasob1210@gmail.com>
2023-02-28 18:32:20 +0100

    add type_id on $fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,7 +10,7 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = [
-        'author', 'slug', 'title', 'date', 'content', 'image'
+        'author', 'slug', 'title', 'date', 'content', 'image', 'type_id'
     ];
 
     public function type()
